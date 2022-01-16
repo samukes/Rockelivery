b0150e0b6b85b07d69a2648e33550f7e1db627f4
samukes <darkdarumia@google.com>
2022-01-16 03:18:25 -0300

    Adjusting test with mox

--- test/rockelivery/users/create_test.exs
+++ test/rockelivery/users/create_test.exs
@@ -1,15 +1,20 @@
 defmodule Rockelivery.Users.CreateTest do
   use Rockelivery.DataCase, async: true
 
+  import Mox
   import Rockelivery.Factory
 
-  alias Rockelivery.User
-  alias Rockelivery.Error
+  alias Rockelivery.{Error, User}
   alias Rockelivery.Users.Create
+  alias Rockelivery.ViaCep.ClientMock
 
   describe "call/1" do
     test "when all params are valid, returns valid user" do
       params = build(:user_params)
+
+      expect(ClientMock, :get_cep_info, fn _cep ->
+        {:ok, build(:cep_info)}
+      end)
 
       response = Create.call(params)
 
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -27,4 +27,19 @@ defmodule Rockelivery.Factory do
       id: "068fee54-3ef7-41d1-b322-2eb7e7d85d4c"
     }
   end
+
+  def cep_info_factory do
+    %{
+      "bairro" => "Sé",
+      "cep" => "01001-000",
+      "complemento" => "lado ímpar",
+      "ddd" => "11",
+      "gia" => "1004",
+      "ibge" => "3550308",
+      "localidade" => "São Paulo",
+      "logradouro" => "Praça da Sé",
+      "siafi" => "7107",
+      "uf" => "SP"
+    }
+  end
 end
